d1159f65949edbfba12f4cf778742d80f47db281
lemakcompany26-ai <lemakcompany26@gmail.com>
2026-09-25 07:51:38 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 
-}import { Suspense, lazy, useEffect } from 'react';
+import { Suspense, lazy, useEffect } from "react";
 import { Toaster } from "@/components/ui/toaster"
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClientInstance } from '@/lib/query-client'
